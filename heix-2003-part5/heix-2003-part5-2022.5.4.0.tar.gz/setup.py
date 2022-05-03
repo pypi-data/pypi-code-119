f3d769618a690da7007a6e4afcaa10b77b3e7028
Tom Forbes <tom@tomforb.es>
2022-05-03 16:18:56 +0000

    {"name":"heix-2003-part5","version":"2022.5.4.0","file":"heix-2003-part5-2022.5.4.0.tar.gz","path":"heix-2003-part5/heix-2003-part5-2022.5.4.0.tar.gz"}

---
+++ heix-2003-part5/heix-2003-part5-2022.5.4.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Heix2003Part5
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Heix2003Part5'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="heix-2003-part5",
+    version=Heix2003Part5.__version__,
+    url="https://github.com/apachecn/heix-2003-part5",
+    author=Heix2003Part5.__author__,
+    author_email=Heix2003Part5.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="黑客X档案合订本2003-2004年（下）第一部分",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "heix-2003-part5=Heix2003Part5.__main__:main",
+            "Heix2003Part5=Heix2003Part5.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
