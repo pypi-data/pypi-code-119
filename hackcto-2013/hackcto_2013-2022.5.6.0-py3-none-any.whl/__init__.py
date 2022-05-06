e8e8e209550a4638f870a7d049ddee5d7ebb656b
Tom Forbes <tom@tomforb.es>
2022-05-06 14:49:08 +0000

    {"name":"hackcto-2013","version":"2022.5.6.0","file":"hackcto_2013-2022.5.6.0-py3-none-any.whl","path":"hackcto-2013/hackcto_2013-2022.5.6.0-py3-none-any.whl"}

---
+++ hackcto-2013/hackcto_2013-2022.5.6.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""hackcto-2013
+https://github.com/apachecn/hackcto-2013"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.6.0"
