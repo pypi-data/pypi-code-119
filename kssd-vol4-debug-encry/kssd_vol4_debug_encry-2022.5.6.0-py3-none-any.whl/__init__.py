c5a98d74822a06c03ad2b1024b88461546b11821
Tom Forbes <tom@tomforb.es>
2022-05-06 13:18:59 +0000

    {"name":"kssd-vol4-debug-encry","version":"2022.5.6.0","file":"kssd_vol4_debug_encry-2022.5.6.0-py3-none-any.whl","path":"kssd-vol4-debug-encry/kssd_vol4_debug_encry-2022.5.6.0-py3-none-any.whl"}

---
+++ kssd-vol4-debug-encry/kssd_vol4_debug_encry-2022.5.6.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kssd-vol4-debug-encry
+https://github.com/apachecn/kssd-vol4-debug-encry"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.6.0"
