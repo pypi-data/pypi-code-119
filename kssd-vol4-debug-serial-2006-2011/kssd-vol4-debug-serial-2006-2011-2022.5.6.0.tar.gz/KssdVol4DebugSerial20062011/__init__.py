1c4b4d1869cfe46b41948ba2103d66b4b39c4e07
Tom Forbes <tom@tomforb.es>
2022-05-06 13:23:47 +0000

    {"name":"kssd-vol4-debug-serial-2006-2011","version":"2022.5.6.0","file":"kssd-vol4-debug-serial-2006-2011-2022.5.6.0.tar.gz","path":"kssd-vol4-debug-serial-2006-2011/kssd-vol4-debug-serial-2006-2011-2022.5.6.0.tar.gz"}

---
+++ kssd-vol4-debug-serial-2006-2011/kssd-vol4-debug-serial-2006-2011-2022.5.6.0.tar.gz/KssdVol4DebugSerial20062011/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kssd-vol4-debug-serial-2006-2011
+https://github.com/apachecn/kssd-vol4-debug-serial-2006-2011"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.6.0"
---
+++ kssd-vol4-debug-serial-2006-2011/kssd-vol4-debug-serial-2006-2011-2022.5.6.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import KssdVol4DebugSerial20062011
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('KssdVol4DebugSerial20062011'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="kssd-vol4-debug-serial-2006-2011",
+    version=KssdVol4DebugSerial20062011.__version__,
+    url="https://github.com/apachecn/kssd-vol4-debug-serial-2006-2011",
+    author=KssdVol4DebugSerial20062011.__author__,
+    author_email=KssdVol4DebugSerial20062011.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="KSSD第4篇调试篇：序列号保护实例分析2006-2011",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "kssd-vol4-debug-serial-2006-2011=KssdVol4DebugSerial20062011.__main__:main",
+            "KssdVol4DebugSerial20062011=KssdVol4DebugSerial20062011.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
