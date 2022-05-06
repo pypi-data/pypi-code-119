ce49ead2f43c46bc2ed22c94f105b6287d65db12
Tom Forbes <tom@tomforb.es>
2022-05-06 13:17:55 +0000

    {"name":"kssd-vol4-debug-demo","version":"2022.5.6.0","file":"kssd-vol4-debug-demo-2022.5.6.0.tar.gz","path":"kssd-vol4-debug-demo/kssd-vol4-debug-demo-2022.5.6.0.tar.gz"}

---
+++ kssd-vol4-debug-demo/kssd-vol4-debug-demo-2022.5.6.0.tar.gz/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import KssdVol4DebugDemo
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('KssdVol4DebugDemo'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="kssd-vol4-debug-demo",
+    version=KssdVol4DebugDemo.__version__,
+    url="https://github.com/apachecn/kssd-vol4-debug-demo",
+    author=KssdVol4DebugDemo.__author__,
+    author_email=KssdVol4DebugDemo.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="KSSD第4篇调试篇：演示版保护技术",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "kssd-vol4-debug-demo=KssdVol4DebugDemo.__main__:main",
+            "KssdVol4DebugDemo=KssdVol4DebugDemo.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)
