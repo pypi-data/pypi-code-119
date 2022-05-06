19fa992c981db3f505b08263d5ab7a266b88ba0b
Tom Forbes <tom@tomforb.es>
2022-05-06 15:17:00 +0000

    {"name":"secbook-1to9","version":"2022.5.6.0","file":"secbook_1to9-2022.5.6.0-py3-none-any.whl","path":"secbook-1to9/secbook_1to9-2022.5.6.0-py3-none-any.whl"}

---
+++ secbook-1to9/secbook_1to9-2022.5.6.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""secbook-1to9
+https://github.com/apachecn/secbook-1to9"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.6.0"
