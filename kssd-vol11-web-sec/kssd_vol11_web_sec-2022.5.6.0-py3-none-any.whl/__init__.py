66ea39960cce2dae370bcf49d29ea904afdaa028
Tom Forbes <tom@tomforb.es>
2022-05-06 13:05:08 +0000

    {"name":"kssd-vol11-web-sec","version":"2022.5.6.0","file":"kssd_vol11_web_sec-2022.5.6.0-py3-none-any.whl","path":"kssd-vol11-web-sec/kssd_vol11_web_sec-2022.5.6.0-py3-none-any.whl"}

---
+++ kssd-vol11-web-sec/kssd_vol11_web_sec-2022.5.6.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kssd-vol11-web-sec
+https://github.com/apachecn/kssd-vol11-web-sec"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.6.0"
