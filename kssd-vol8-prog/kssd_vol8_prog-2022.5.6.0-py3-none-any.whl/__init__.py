c5d8c9c9e5f900d1d667ff673825eece89a1075e
Tom Forbes <tom@tomforb.es>
2022-05-06 13:39:45 +0000

    {"name":"kssd-vol8-prog","version":"2022.5.6.0","file":"kssd_vol8_prog-2022.5.6.0-py3-none-any.whl","path":"kssd-vol8-prog/kssd_vol8_prog-2022.5.6.0-py3-none-any.whl"}

---
+++ kssd-vol8-prog/kssd_vol8_prog-2022.5.6.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""kssd-vol8-prog
+https://github.com/apachecn/kssd-vol8-prog"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.6.0"
