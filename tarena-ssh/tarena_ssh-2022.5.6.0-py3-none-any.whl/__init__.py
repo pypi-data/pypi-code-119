283d8c809eb4dbec65f37532a16c71e26dc021a0
Tom Forbes <tom@tomforb.es>
2022-05-06 14:06:17 +0000

    {"name":"tarena-ssh","version":"2022.5.6.0","file":"tarena_ssh-2022.5.6.0-py3-none-any.whl","path":"tarena-ssh/tarena_ssh-2022.5.6.0-py3-none-any.whl"}

---
+++ tarena-ssh/tarena_ssh-2022.5.6.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""tarena-ssh
+https://github.com/apachecn/tarena-ssh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.5.6.0"
